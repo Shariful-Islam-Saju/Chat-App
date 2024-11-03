7e15f34fbff6378a76d97192aa3e57af25a6ddf0
Shariful Islam <sajukhan12905@gmail.com>
2024-11-03 22:37:16 +0600

    make jwt token

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,3 +1,49 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import createError from "http-errors";
+
+import People from "../model/people.js";
+
 export function getLogin(req, res, next) {
   res.render("index");
+}
+
+export async function login(req, res, next) {
+  try {
+    const user = await People.findOne({
+      $or: [{ email: req.body.username }, { mobile: req.body.username }],
+    });
+
+    if (user && user._id) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+
+      if (isValidPassword) {
+        const userObject = {
+          name: user.name,
+          email: user.email,
+          mobile: user.mobile,
+          role: "user",
+        };
+        const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRY,
+        });
+        res.cookie(process.env.COOKIE_NAME, token, {
+          httpOnly: true,
+          maxAge: parseInt(process.env.JWT_EXPIRY, 10),
+          signed: true, // Fixed typo
+          secure: process.env.NODE_ENV === "production", // Set to true in production
+        });
+
+        res.locals.loggedInUser = userObject;
+        return res.render("index");
+      }
+    }
+
+    throw createError(401, "Login Failed!");
+  } catch (error) {
+    next(error); // Call next with the error for middleware handling
+  }
 }
