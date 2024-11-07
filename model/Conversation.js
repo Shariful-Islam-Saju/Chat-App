5c85125ffd729ecaa117facc9eabf998fbaec94d
Shariful Islam <sajukhan12905@gmail.com>
2024-11-07 12:56:00 +0600

    make some change in inbox

---
+++ model/Conversation.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+
+const conversationSchema = mongoose.Schema(
+  {
+    creator: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+
+    participant: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+    last_updated: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+export default Conversation;
