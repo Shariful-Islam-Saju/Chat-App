d95a0d22eba5c8f1becebdd6eac7f68bcd3c3651
Shariful Islam <sajukhan12905@gmail.com>
2024-11-10 00:00:18 +0600

    completed tutorials 5

--- middleware/inbox/atachmentUpload.js
+++ middleware/inbox/atachmentUpload.js
@@ -11,7 +11,6 @@ function attachmentUpload(req, res, next) {
   // call the middleware function
   upload.any()(req, res, (err) => {
     if (err) {
-      console.log(err)
       res.status(500).json({
         errors: {
           avatar: {
